09778ab196d55c09705babc48ba03ba6417bc667
Zarisa <jannesari.zarisa@gmail.com>
2022-05-26 23:17:44 +0430

    improve: exception handling for register part

--- app/src/main/java/com/zarisa/statuswork/data/user/UserRemoteDataSource.kt
+++ app/src/main/java/com/zarisa/statuswork/data/user/UserRemoteDataSource.kt
@@ -1,6 +1,8 @@
 package com.zarisa.statuswork.data.user
 
 import com.zarisa.statuswork.data.network.LoginService
+import com.zarisa.statuswork.model.ApiState
+import com.zarisa.statuswork.model.Resource
 import com.zarisa.statuswork.model.User
 
 class UserRemoteDataSource(var loginApiService: LoginService) {
@@ -8,6 +10,25 @@ class UserRemoteDataSource(var loginApiService: LoginService) {
     suspend fun registerUser(user: User): User {
         return loginApiService.registerUser(user)
     }
+//
+//    suspend fun getUser(id: String): Resource<User> {
+//
+//
+//        var result = Resource<User>(ApiState.LOADING , null)
+//        try {
+//            var response = loginApiService.getUser(id)
+//            if(response.isSuccessful){
+//                result.data = response.body() as User
+//                result.status = ApiState.DONE
+//            }else
+//                return Resource(ApiState.ERROR , null , response.message())
+//        }
+//        catch (ex: Exception){
+//            return Resource(ApiState.ERROR , null , ex.message)
+//        }
+//        return result
+//    }
+
 
     suspend fun getUser(id: String): User {
         return loginApiService.getUser(id)
