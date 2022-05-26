663cf575d6c26419b6ea66f6a6630db4bd1dc160
Zarisa <jannesari.zarisa@gmail.com>
2022-05-26 10:06:46 +0430

    inherent application to start coin

---
+++ app/src/main/java/com/zarisa/statuswork/App.kt
@@ -0,0 +1,18 @@
+package com.zarisa.statuswork
+
+import android.app.Application
+import com.zarisa.statuswork.domain.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin{
+            androidLogger()
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
