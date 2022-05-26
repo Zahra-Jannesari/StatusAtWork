598938db8f2d77c861dba9f6bd9a165bd8d3c79e
Zarisa <jannesari.zarisa@gmail.com>
2022-05-26 12:56:59 +0430

    add register view model

---
+++ app/src/main/java/com/zarisa/statuswork/ui/register/RegisterviewModel.kt
@@ -0,0 +1,8 @@
+package com.zarisa.statuswork.ui.register
+
+import androidx.lifecycle.ViewModel
+import com.zarisa.statuswork.data.user.UserRepository
+
+class RegisterViewModel(userRepository: UserRepository) : ViewModel(){
+
+}
